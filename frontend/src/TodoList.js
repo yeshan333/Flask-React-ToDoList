169bed290b14ef494b9c979aa318ad3378af4b2f
yeshan333 <1329441308@qq.com>
2020-01-16 01:06:31 +0800

    feat: frontend app ADD UPDATE feature

--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -6,34 +6,72 @@ class TodoList extends Component {
         super(props);
         this.state = {
             inputValue: '',
+            //isEdit: true,//judge is edit?
             items: [
-                "新年",
-                "你好"
+                {'id': 't66', 'value': 'nihao', 'isEditing': false},
+                {'id': 't77', 'value': 'wocao', 'isEditing': true},
             ]
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSendClick = this.handleSendClick.bind(this);
         this.handleDeleteItem = this.handleDeleteItem.bind(this);
+        this.handleEditItem = this.handleEditItem.bind(this);
+        this.handleReEditSend = this.handleReEditSend.bind(this);
     }
 
+    //input输入监控
     handleChange(event) {
         this.setState({
             inputValue: event.target.value
         })
     }
 
+    //提交事件
     handleSendClick(event) {
-        this.setState({
-            items: [this.state.inputValue, ...this.state.items]
-        })
+        if(this.state.inputValue){
+            let new_item = {
+                'id': 't'+Math.random().toString ,
+                'value': this.state.inputValue,
+                'isEditing': false
+            }
+            this.setState({
+                inputValue: '',
+                items: [new_item, ...this.state.items]
+            })
+        }else{
+            alert("Please enter value!")
+        }
     }
 
+    //删除列表项事件
     handleDeleteItem(index) {
         let new_items = this.state.items;
         new_items.splice(index, 1);
         this.setState({
             items: new_items
         })
+    }
+
+    //编辑列表项事件
+    handleEditItem(index) {
+        let new_items = this.state.items;
+        new_items[index].isEditing = true;
+        this.setState({
+            items: new_items
+        });
+    }
+
+    handleReEditSend(index, value) {
+        if(value){
+            let new_items = this.state.items;
+            new_items[index].isEditing = false;
+            new_items[index].value = value;
+            this.setState({
+                items: new_items
+            });
+        }else{
+            alert("Please enter value!")
+        }
     }
 
     render() {
@@ -45,6 +83,7 @@ class TodoList extends Component {
                     <input
                         value={this.state.inputValue}
                         onChange={this.handleChange}
+                        onKeyPress={this.handleSendClick}
                     />
                     <button onClick={this.handleSendClick}>Send</button>
                 </div>
@@ -53,10 +92,12 @@ class TodoList extends Component {
                         this.state.items.map((item, index) => {
                             return (
                                 <TodoListItem
-                                    key={item+index}
+                                    key={item.id+index}
                                     item={item}
                                     index={index}
                                     handleDeleteItem={this.handleDeleteItem}
+                                    handleEditItem={this.handleEditItem}
+                                    handleReEditSend={this.handleReEditSend}
                                 />
                             );
                         })
--- frontend/src/TodoListItem.js
+++ frontend/src/TodoListItem.js
@@ -3,22 +3,62 @@ import React, {Component, Fragment} from 'react'
 class TodoListItem extends Component {
     constructor(props){
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        const {item} = this.props;
+        this.state = {
+            inputValue : item.value
+        };
+
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
+        this.handleReEditSend = this.handleReEditSend.bind(this);
     }
 
-    handleClick() {
+    //删除事项
+    handleDeleteClick() {
         this.props.handleDeleteItem(this.props.index);
     }
 
+    handleEditClick() {
+        this.props.handleEditItem(this.props.index)
+    }
+
+    //监听编辑修改
+    handleInputChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+    }
+
+    //再编辑提交
+    handleReEditSend() {
+        this.props.handleReEditSend(this.props.index, this.state.inputValue);
+    }
+
     render() {
-        return (
-            <Fragment>
-                <li>
-                    <button onClick={this.handleClick}>delete</button>
-                    {this.props.item}
-                </li>
-            </Fragment>
-        );
+        if(!this.props.item.isEditing){
+            return (
+                <Fragment>
+                    <li>
+                        <button onClick={this.handleDeleteClick}>delete</button>
+                        <button onClick={this.handleEditClick}>edit</button>
+                        {this.props.item.value}
+                    </li>
+                </Fragment>
+            );
+        }else{
+            return (
+                <Fragment>
+                    <li>
+                        <input
+                            value={this.state.inputValue}
+                            onChange={this.handleInputChange}
+                            onKeyPress={this.handleReEditSend}
+                        />
+                    </li>
+                </Fragment>
+            );
+        }
     }
 }
 
