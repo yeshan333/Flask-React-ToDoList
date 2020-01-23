e16e90b272589bed986d0ae7c4c23552bff39d09
yeshan333 <1329441308@qq.com>
2020-01-23 16:05:03 +0800

    feat: initial production env for heroku

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Flask-React-ToDoList
+```bash
+\MongoDB> .\mongod.exe --config .\mongod.conf # start mongodb service
+
+nose2 # start test
+
+
+```
--- frontend/src/__tests__/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoList from '../TodoList';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TodoList />, div);
-});
